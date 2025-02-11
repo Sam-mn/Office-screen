a8b81f5d11057de1dd65fefcbcf084e10d4eb08d
danielmichaneck <daniel.michaneck@gmail.com>
2025-02-11 21:55:40 +0100

    Added login functionality to login page.

--- Client/src/Pages/LoginPage.tsx
+++ Client/src/Pages/LoginPage.tsx
@@ -1,10 +1,28 @@
+import { FormEventHandler, useState } from "react";
 import "../css/LoginPage.css";
+import login from "../utils/requests";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const navigate = useNavigate();
+
+  const attemptLogin: FormEventHandler<HTMLFormElement> = async (form) => {
+    form.preventDefault();
+    try {
+      const token = await login(username, password);
+      navigate("/")
+    }
+    catch {
+      throw new Error("Login failed.");
+    }
+  }
+
   return (
     <div className="login-container">
       <h1>Login</h1>
-      <form>
+      <form onSubmit={attemptLogin}>
         <div className="container">
           <label htmlFor="uname">
             <b>Username</b>
@@ -13,6 +31,7 @@ const LoginPage = () => {
             type="text"
             placeholder="Enter Username"
             name="uname"
+            onChange={(name) => setUsername(name.target.value)}
             required
           />
 
@@ -23,6 +42,7 @@ const LoginPage = () => {
             type="password"
             placeholder="Enter Password"
             name="psw"
+            onChange={(pw) => setPassword(pw.target.value)}
             required
           />
           <button type="submit" className="login-button ">
